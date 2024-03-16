1b59d9cc2989001898db27c7e945c59be95179cc
AliceGonzalez <alicegcodes@gmail.com>
2024-03-16 19:55:07 -0400

    First method completed

--- docs/matuszek/Hammurabi.java
+++ docs/matuszek/Hammurabi.java
@@ -1,150 +1,61 @@
 package hammurabi.docs.matuszek;
-import java.io.IOException;
-import java.util.*;
 
-public class Hammurabi {
-    static int totalDeaths = 0, percentDied = 0, year = 0, population = 95, stores = 2800, immigrants = 5, deaths,
-            harvest = 3000, yeild = 3, acres = harvest / yeild, eaten = harvest - stores, landPrice, fullPeople, temp;
-    static boolean plague = false;
-    final static String FINK = "DUE TO THIS EXTREME MISMANAGEMENT YOU HAVE NOT ONLY\n" +
-            "BEEN IMPEACHED AND THROWN OUT OF OFFICE BUT YOU HAVE\n" +
-            "ALSO BEEN DECLARED PERSONA NON GRATA!!\n";
-    Scanner input = new Scanner(System.in);
+import java.util.Random;         // imports go here
+import java.util.Scanner;
+public class Hammurabi {         // must save in a file named Hammurabi.java
+    Random rand = new Random();  // this is an instance variable
+    Scanner scanner = new Scanner(System.in);
 
-    private void newYear() {
-        year += 1;
-        population += immigrants;
-        landPrice = (int) (10 * Math.random() + 17);
-        System.out.println(report());
-        do {
-            System.out.print("HOW MANY ACRES DO YOU WISH TO BUY?  ");
-            temp = input.nextInt();
-            if (temp < 0)
-                epicFail(0);
-            if (temp * landPrice > stores)
-                System.out.println("HAMURABI:  THINK AGAIN. YOU HAVE ONLY\n" +
-                        stores + " BUSHELS OF GRAIN. NOW THEN,");
-        } while (temp * landPrice > stores);
-        acres += temp;
-        stores -= temp * landPrice;
-        do {
-            System.out.print("HOW MANY ACRES DO YOU WISH TO SELL?  ");
-            temp = input.nextInt();
-            if (temp < 0)
-                epicFail(0);
-            if (temp > acres)
-                System.out.println("HAMURABI:  THINK AGAIN. YOU OWN ONLY " + acres + " ACRES. NOW THEN,");
-        } while (temp > acres);
-        stores += temp * landPrice;
-        acres -= temp;
-        do {
-            System.out.print("\nHOW MANY BUSHELS DO YOU WISH TO FEED YOUR PEOPLE?  ");
-            temp = input.nextInt();
-            if (temp < 0)
-                epicFail(0);
-            if (temp > stores)
-                System.out.println("HAMURABI:  THINK AGAIN. YOU HAVE ONLY\n" +
-                        stores + " BUSHELS OF GRAIN. NOW THEN,");
-        } while (temp > stores);
-        fullPeople = temp / 20;
-        stores -= temp;
-        do {
-            System.out.print("\nHOW MANY ACRES DO YOU WISH TO PLANT WITH SEED?  ");
-            temp = input.nextInt();
-            if (temp < 0)
-                epicFail(0);
-            if (temp > acres)
-                System.out.println("HAMURABI:  THINK AGAIN. YOU OWN ONLY " + acres + " ACRES. NOW THEN,");
-            if (temp / 2 > stores)
-                System.out.println("HAMURABI:  THINK AGAIN. YOU HAVE ONLY\n" +
-                        stores + " BUSHELS OF GRAIN. NOW THEN,");
-            if (temp > population * 10)
-                System.out.println("BUT YOU HAVE ONLY" + population + "PEOPLE TO TEND THE FIELDS. NOW THEN,");
-        } while (temp > acres || temp / 2 > stores || temp > population * 10);
-        stores -= temp / 2;
-        yeild = (int) (Math.random() * 5 + 1);
-        harvest = temp * yeild;
-        temp = (int) (Math.random() * 5 + 1);
-        if (temp % 2 != 1)
-            eaten = (stores / temp);
-        else
-            eaten = 0;
-        stores += (harvest - eaten);
-        immigrants = (int) (Math.random() * 5 + 1) *
-                (20 * acres + stores) / population / 100 + 1;
-        if (population > fullPeople) {
-            deaths = population - fullPeople;
-            if (deaths > .45 * population)
-                epicFail(1);
-            percentDied = ((year - 1) * percentDied + deaths * 100 / population) / year;
-            population = fullPeople;
-            totalDeaths += deaths;
-        }
-        if (20 * Math.random() >= 17)
-            plague = true;
-        plague = false;
+    public static void main(String[] args) { // required in every Java program
+        new Hammurabi().playGame();
     }
 
-    private static String report() {
-        String answer = "\nHAMURABI:  I BEG TO REPORT TO YOU,\n" +
-                "IN YEAR " + year + ", " + deaths + " PEOPLE STARVED, " + immigrants + " CAME TO THE CITY.\n";
-        if (plague) {
-            population = population / 2;
-            answer += "A HORRIBLE PLAGUE STRUCK!  HALF THE PEOPLE DIED.\n";
-        }
-        answer += "POPULATION IS NOW " + population + ".\n" +
-                "THE CITY NOW OWNS " + acres + " ACRES.\n" +
-                "YOU HARVESTED " + yeild + " BUSHELS PER ACRE.\n" +
-                "RATS ATE " + eaten + " BUSHELS.\n" +
-                "YOU NOW HAVE " + stores + " BUSHELS IN STORE\n\n" +
-                "LAND IS TRADING AT " + landPrice + " BUSHELS PER ACRE.";
-        return answer;
+    void playGame() {
+        askHowManyAcresToBuy(landValue,bushels);
+
+    }
+
+    // declare local variables here: grain, population, etc.
+    int population = 100;
+    int bushels = 280;
+    int acres = 1000;
+    int landValue = 19;
+
+    // statements go after the declarations
+
+    int askHowManyAcresToBuy(int price, int bushels){
+        // Asks the player how many acres of land to buy, and returns that number. You must have enough grain to pay for your purchase.
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("HOW MANY ACRES DO YOU WISH TO BUY?  ");
+        Integer userInput = scanner.nextInt();
+
+        return userInput;
     }
 
 
-    private static void epicFail(int x) {
-        String reason = "";
-        switch (x) {
-            case 0: reason = "HAMURABI:  I CANNOT DO WHAT YOU WISH.\n" +
-                    "GET YOURSELF ANOTHER STEWARD!!!!!"; break;
-            case 1: reason = "YOU STARVED " + deaths + " PEOPLE IN ONE YEAR!!!\n" +
-                    FINK; break;
-        }
-        System.out.println(reason);
-        System.exit(0);
-    }
-
-    private void finished() {
-        String answer = "IN YOUR 10-YEAR TERM OF OFFICE, " + percentDied + " PERCENT OF THE\n" +
-                "POPULATION STARVED PER YEAR ON AVERAGE, I.E., A TOTAL OF\n" +
-                totalDeaths + " PEOPLE DIED!!\n" +
-                "YOU STARTED WITH 10 ACRES PER PERSON AND ENDED WITH\n" +
-                acres / population + " ACRES PER PERSON\n\n";
-        if (percentDied > 33 || acres / population < 7)
-            answer += FINK;
-        else if (percentDied > 10 || acres / population < 9)
-            answer += "YOUR HEAVY-HANDED PERFORMANCE SMACKS OF NERO AND IVAN IV.\n" +
-                    "THE PEOPLE (REMAINING) FIND YOU AN UNPLEASANT RULER, AND,\n" +
-                    "FRANKLY, HATE YOUR GUTS!";
-        else if (percentDied > 3 || acres / population < 10)
-            answer += "YOUR PERFORMANCE COULD HAVE BEEN SOMEWHAT BETTER, BUT\n" +
-                    "REALLY WASN'T TOO BAD AT ALL.\n" +
-                    Math.random() * population * .8 + " PEOPLE WOULD" +
-                    "DEARLY LIKE TO SEE YOU ASSASSINATED BUT WE ALL HAVE OUR" +
-                    "TRIVIAL PROBLEMS";
-        else
-            answer += "A FANTASTIC PERFORMANCE!!!  CHARLEMANGE, DISRAELI, AND\n" +
-                    "JEFFERSON COMBINED COULD NOT HAVE DONE BETTER!";
-        answer += "\n\n\n\n\n\n\n\n\n\nSo long for now.";
-        System.out.println(answer);
-    }
-
-    public static void main(String[] args) throws IOException {
-        Hammurabi a = new Hammurabi();
-        System.out.println("\t\t\t\tHAMURABI\n\t       CREATIVE COMPUTING MORRISTOWN, NEW JERSEY\n\n" +
-                "TRY YOUR HAND AT GOVERNING ANCIENT SUMERIA\nSUCCESSFULLY FOR A TEN-YEAR TERM OF OFFICE.");
-        while (year < 10)
-            a.newYear();
-        a.finished();
-    }
+//        int askHowManyAcresToSell(int acresOwned){
+//            //Asks the player how many acres of land to sell, and returns that number. You can't sell more than you have.Do not ask this question if the player is buying land; it doesn't make sense to do both in one turn
+//        }
+//
+//
+//        int askHowMuchGrainToFeedPeople(int bushels)
+//        {//Ask the player how much grain to feed people, and returns that number. You can't feed them more grain than you have. You can feed them more than they need to survive.
+//        }
+//
+//        int askHowManyAcresToPlant(int acresOwned, int population, int bushels){
+//            //Ask the player how many acres to plant with grain, and returns that number. You must have enough acres, enough grain, and enough people to do the planting. Any grain left over goes into storage for next year.
+//        }
+//        //other methods go here
+//        public void yearlySummary(){
+//            System.out.println("O great Hammurabi!\n" +
+//                    "You are in year 1 of your ten year rule.\n" +
+//                    "In the previous year 0 people starved to death.\n" +
+//                    "In the previous year 5 people entered the kingdom.\n" +
+//                    "The population is now 100.\n" +
+//                    "We harvested 3000 bushels at 3 bushels per acre.\n" +
+//                    "Rats destroyed 200 bushels, leaving 2800 bushels in storage.\n" +
+//                    "The city owns 1000 acres of land.\n" +
+//                    "Land is currently worth 19 bushels per acre.");
+//        }
 }
+
