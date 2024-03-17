2b402fe42127afee39e94c914e552b49d8c954c4
AliceGonzalez <alicegcodes@gmail.com>
2024-03-16 23:03:15 -0400

    Starvation Test Method Completed

--- docs/matuszek/Hammurabi.java
+++ docs/matuszek/Hammurabi.java
@@ -1,12 +1,9 @@
 package hammurabi.docs.matuszek;
 
 import java.util.InputMismatchException;
-import java.util.Random;         // imports go here
+import java.util.Random;
 import java.util.Scanner;
-public class Hammurabi {         // must save in a file named Hammurabi.java
-    Random rand = new Random();  // this is an instance variable
-    Scanner scanner = new Scanner(System.in);
-
+public class Hammurabi {
     public static void main(String[] args) { // required in every Java program
         new Hammurabi().playGame();
     }
@@ -19,10 +16,9 @@ public class Hammurabi {         // must save in a file named Hammurabi.java
         askHowMuchGrainToFeedPeople(bushels);
         askHowManyAcresToPlant(population, acres, bushels);
         yearlySummary();
-
     }
 
-    // declare local variables here: grain, population, etc.
+    Random random = new Random();  // this is an instance variable
     int year;
     int population = 100;
     int bushels = 280;
@@ -31,7 +27,6 @@ public class Hammurabi {         // must save in a file named Hammurabi.java
     boolean plague;
     //static int totalDeaths = 0, percentDied = 0, year = 0, population = 95, stores = 2800, immigrants = 5, deaths, harvest = 3000, yeild = 3, acres = harvest / yeild, eaten = harvest - stores, landPrice, fullPeople, temp;
 
-    // statements go after the declarations
     void announceGame() {
         System.out.println("Congratulations, you are the newest ruler of ancient Sumer, elected for a ten year term of office. Your duties are to dispense food, direct farming, and buy and sell land as needed to support your people. Watch out for rat infestiations and the plague! Grain is the general currency, measured in bushels. The following will help you in your decisions:\n" +
                 "\n" +
@@ -60,8 +55,6 @@ public class Hammurabi {         // must save in a file named Hammurabi.java
             }
         }
     }
-
-
     int askHowManyAcresToSell(int acresOwned) {
         //Asks the player how many acres of land to sell, and returns that number. You can't sell more than you have.Do not ask this question if the player is buying land; it doesn't make sense to do both in one turn
         Scanner scanner = new Scanner(System.in);
@@ -81,43 +74,62 @@ public class Hammurabi {         // must save in a file named Hammurabi.java
         }
 
     }
-
     int askHowMuchGrainToFeedPeople(int bushels) {//Ask the player how much grain to feed people, and returns that number. You can't feed them more grain than you have. You can feed them more than they need to survive.
         Scanner scanner = new Scanner(System.in);
-        System.out.print("HOW MANY BUSHELS DO YOU WISH TO FEED YOUR PEOPLE?  ");
-        Integer userInput = scanner.nextInt();
-
-        while (userInput < bushels) {
-            return userInput;
+        while (true) {
+            try {
+                System.out.print("HOW MANY BUSHELS DO YOU WISH TO FEED YOUR PEOPLE?  ");
+                Integer userInput = scanner.nextInt();
+                if (userInput <= bushels) {
+                    return userInput;
+                } else {
+                    System.out.println("HAMURABI:  THINK AGAIN. YOU HAVE ONLY\n" + bushels + " BUSHELS OF GRAIN. NOW THEN,");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Please enter a valid number!");
+                scanner.next();
+            }
         }
-
-        System.out.println("HAMURABI:  THINK AGAIN. YOU HAVE ONLY\n" + bushels + " BUSHELS OF GRAIN. NOW THEN,");
-
-        return 0;
     }
-
     int askHowManyAcresToPlant(int acresOwned, int population, int bushels) {
         //Ask the player how many acres to plant with grain, and returns that number. You must have enough acres, enough grain, and enough people to do the planting. Any grain left over goes into storage for next year.
         Scanner scanner = new Scanner(System.in);
-        System.out.print("HOW MANY ACRES DO YOU WISH TO PLANT WITH SEED?  ");
-        Integer userInput = scanner.nextInt();
-
-        while (userInput < acres) {
-            return userInput;
+        while (true) {
+            try {
+                System.out.print("HOW MANY ACRES DO YOU WISH TO PLANT WITH SEED?  ");
+                Integer userInput = scanner.nextInt();
+                if (userInput <= acres) {
+                    return userInput;
+                } else {
+                    System.out.println("HAMURABI:  THINK AGAIN. YOU OWN ONLY " + acres + " ACRES. NOW THEN,");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Please enter a valid number!");
+                scanner.next();
+            }
         }
-        System.out.println("HAMURABI:  THINK AGAIN. YOU OWN ONLY " + acres + " ACRES. NOW THEN,");
 
-        return 0;
     }
 
     int plagueDeaths(int population){
         //Each year, there is a 15% chance of a horrible plague. When this happens, half your people die. Return the number of plague deaths (possibly zero).}
-        return 0;
+        return population / 2;
     }
 
     int starvationDeaths(int population, int bushelsFedToPeople) {
         //Each person needs 20 bushels of grain to survive. If you feed them more than this, they are happy, but the grain is still gone. You don't get any benefit from having happy subjects. Return the number of deaths from starvation (possibly zero).
-        return 0;
+        int peopleFed = 0;
+        int bushelCounter = bushelsFedToPeople;
+
+        for (int i = 0; i <= population; i++ ){
+            if(bushelCounter >= 20){
+                peopleFed++;
+                bushelCounter -= 20;
+            }
+        }
+
+        int peopleStarved = population - peopleFed;
+        return peopleStarved;
     }
 
     boolean uprising(int population, int howManyPeopleStarved){
@@ -140,8 +152,6 @@ public class Hammurabi {         // must save in a file named Hammurabi.java
         return 0;
     }
 
-
-
     int newCostOfLand() {
         //The price of land is random, and ranges from 17 to 23 bushels per acre. Return the new price for the next set of decisions the player has to make. (The player will need this information in order to buy or sell land.)
         return 0;
@@ -157,7 +167,5 @@ public class Hammurabi {         // must save in a file named Hammurabi.java
                 "The city owns 1000 acres of land.\n" +
                 "Land is currently worth 19 bushels per acre.");
     }
-
-
 }
 
